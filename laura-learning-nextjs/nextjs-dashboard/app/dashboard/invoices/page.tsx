61aee447edf16ede645f7307ff8eace2f12c4a19
ljmullings <lalamullings@icloud.com>
2024-10-11 16:21:00 -0500

    adds pagination support for invoice component

--- laura-learning-nextjs/nextjs-dashboard/app/dashboard/invoices/page.tsx
+++ laura-learning-nextjs/nextjs-dashboard/app/dashboard/invoices/page.tsx
@@ -5,6 +5,7 @@ import { CreateInvoice } from '@/app/ui/invoices/buttons';
 import { lusitana } from '@/app/ui/fonts';
 import { Suspense } from 'react';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
+import { fetchInvoicesPages } from '@/app/lib/data';
 
 export default async function Page({
   searchParams,
@@ -14,9 +15,9 @@ export default async function Page({
     page?: string;
   };
 }) {
-
   const query = searchParams?.query || '';
   const currentPage = Number(searchParams?.page) || 1;
+  const totalPages = await fetchInvoicesPages(query);
 
   return (
     <div className="w-full">
@@ -31,7 +32,7 @@ export default async function Page({
         <Table query={query} currentPage={currentPage} />
       </Suspense>
       <div className="mt-5 flex w-full justify-center">
-        {/* <Pagination totalPages={totalPages} /> */}
+        {<Pagination totalPages={totalPages} /> }
       </div>
     </div>
   );
--- laura-learning-nextjs/nextjs-dashboard/app/ui/search.tsx
+++ laura-learning-nextjs/nextjs-dashboard/app/ui/search.tsx
@@ -14,6 +14,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
     console.log(`Searching... ${term}`);
 
     const params = new URLSearchParams(searchParams.toString());
+    params.set('page', '1');
     if (term) {
       params.set('query', term);
     } else {
